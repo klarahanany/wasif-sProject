e5282c0dc063c1d0faa495a36ab44623d2a15faf
Wasif <wasifanton@gmail.com>
2024-03-08 21:12:23 +0200

    Added comments

--- middleware/authMiddware.js
+++ middleware/authMiddware.js
@@ -1,99 +1,112 @@
+// JSON WEB TOKEN AND USER MODEL MODULES
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-//verifying the token for any route we choose to protect
+// User
+// MIDDLEWARE FOR REQUIRING AUTHENTICATION
+// Verify the token for any route we choose to protect
 const requireAuth = (req, res, next) => {
-  const token = req.cookies.jwt; //get the token from cookie (if user is already signed)
-  //check jwt exists and is verified
+  const token = req.cookies.jwt; // Get the token from the cookie (if the user is already signed in)
+
+  // Check if JWT exists and is verified
   if (token) {
     jwt.verify(token, "mysecretcode", (err, decodedToken) => {
       if (err) {
-        //if we have token but not valid
+        // If the token is present but not valid
         console.log(err.message);
         res.redirect("/login");
       } else {
-        //is valid user
+        // Valid user
         console.log(decodedToken);
         next();
       }
     });
   } else {
-    //the user isn't logged in
+    // The user isn't logged in
     res.redirect("/loginError");
     res.status(400).end();
   }
 };
-//verifying the token for any route we choose to protect
+
+// Admin
+// MIDDLEWARE FOR REQUIRING ADMIN AUTHENTICATION
+// Verify the admin token for any route we choose to protect
 const requireAuthAdmin = (req, res, next) => {
-  const token = req.cookies.jwtAdmin; //get the token from cookie (if user is already signed)
-  //check jwt exists and is verified
+  const token = req.cookies.jwtAdmin; // Get the token from the cookie (if the admin is already signed in)
+
+  // Check if JWT exists and is verified
   if (token) {
     jwt.verify(token, "mysecretcode", (err, decodedToken) => {
       if (err) {
-        //if we have token but not valid
+        // If the token is present but not valid
         console.log(err.message);
         res.redirect("/admin");
       } else {
-        //is valid user
+        // Valid admin user
         console.log(decodedToken);
         next();
       }
     });
   } else {
-    //the user isn't logged in
+    // The admin user isn't logged in
     res.redirect("/admin/loginErrorAdmin");
     res.status(400).end();
   }
 };
-//check current user
+
+// MIDDLEWARE TO CHECK CURRENT USER
 const currentUser = (req, res, next) => {
-  const token = req.cookies.jwt; //get the token from cookie (if user is already signed)
-  //check jwt exists and is verified
+  const token = req.cookies.jwt; // Get the token from the cookie (if the user is already signed in)
+
+  // Check if JWT exists and is verified
   if (token) {
-    //verify the token and then u get decoded token on callback function which has in it
-    //saved user id
+    // Verify the token, and then get the decoded token on the callback function which has the saved user ID
     jwt.verify(token, "mysecretcode", async (err, decodedToken) => {
       if (err) {
-        //if we have token but not valid
+        // If the token is present but not valid
         console.log(err.message);
         res.locals.user = null;
         next();
       } else {
-        //is valid user
+        // Valid user
         console.log(decodedToken);
         res.locals.user = await userModel.findById(decodedToken.id);
         next();
       }
     });
   } else {
+    // No user logged in
     res.locals.user = null;
     next();
   }
 };
-//check current user
+
+// MIDDLEWARE TO CHECK CURRENT ADMIN USER
 const currentAdminUser = (req, res, next) => {
-  const token = req.cookies.jwtAdmin; //get the token from cookie (if user is already signed)
-  //check jwt exists and is verified
+  const token = req.cookies.jwtAdmin; /// Get the token from the cookie (if the admin is already signed in)
+  // If the token is present but not valid
   if (token) {
-    //verify the token and then u get decoded token on callback function which has in it
-    //saved user id
+    // Verify the token, and then get the decoded token on the callback function which has the saved user ID
     jwt.verify(token, "mysecretcode", async (err, decodedToken) => {
       if (err) {
-        //if we have token but not valid
+        // If the token is present but not valid
         console.log(err.message);
         res.locals.admin = null;
         next();
       } else {
-        //is valid user
+        // Valid admin user
         console.log(decodedToken);
         res.locals.admin = await userModel.findById(decodedToken.id);
         next();
       }
     });
   } else {
+    // No admin user logged in
+
     res.locals.admin = null;
     next();
   }
 };
 
+// EXPORT MODULES FOR USE IN OTHER FILES
 module.exports = { requireAuth, currentUser, currentAdminUser, requireAuthAdmin };
