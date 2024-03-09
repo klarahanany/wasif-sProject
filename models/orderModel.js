4ddd8b0e77aad3dc860bf251c5e77b18d0be91ac
Wasif <wasifanton@gmail.com>
2024-03-10 00:15:04 +0200

    Added comments

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
-
 const { Schema } = mongoose;
 
+// Define the order schema using Mongoose
 const orderSchema = new Schema({
+  // Reference to the user who placed the order
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+    ref: "user", // Reference to the "user" model
     required: true,
   },
+  // Order date, defaults to the current date and time
   order_date: {
     type: Date,
     default: Date.now,
   },
+  // Array of items in the order, each item includes productId and quantity
   items: [
     {
       productId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
+        ref: "Product", // Reference to the "Product" model
         required: true,
       },
       quantity: {
@@ -25,10 +28,12 @@ const orderSchema = new Schema({
       },
     },
   ],
+  // Total amount for the order
   total_amount: {
     type: Number,
     required: true,
   },
+  // Payment details
   payment: {
     card_number: {
       type: String,
@@ -41,13 +46,16 @@ const orderSchema = new Schema({
       required: true,
     },
   },
+  // Order status with default set to "pending"
   status: {
     type: String,
-    enum: ["pending", "Ready", "delivered"],
+    enum: ["pending", "Ready", "delivered"], // Allowed values for the status
     default: "pending",
   },
 });
 
+// Create a Mongoose model named "Order" based on the orderSchema
 const Order = mongoose.model("Order", orderSchema);
 
+// Export the Order model to be used in other parts of the application
 module.exports = Order;
