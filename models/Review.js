19c9be21c56e26d3cc0b8aa03ac160e1b2118bb8
Wasif <wasifanton@gmail.com>
2024-03-10 00:18:21 +0200

    Added comments

--- models/Review.js
+++ models/Review.js
@@ -1,30 +1,38 @@
 const mongoose = require("mongoose");
 
+// Define the review schema using Mongoose
 const reviewSchema = new mongoose.Schema({
+  // Title of the review, required field
   title: {
     type: String,
     required: true,
   },
+  // Rating of the review, required with a minimum of 1 and a maximum of 5
   rating: {
     type: Number,
     required: true,
     min: 1,
     max: 5,
   },
+  // Author of the review, required field
   author: {
     type: String,
     required: true,
   },
+  // Date of the review, defaults to the current date and time
   date: {
     type: Date,
     default: Date.now,
   },
+  // Body or content of the review, required field
   body: {
     type: String,
     required: true,
   },
 });
 
+// Create a Mongoose model named "Review" based on the reviewSchema
 const Review = mongoose.model("Review", reviewSchema);
 
+// Export the Review model to be used in other parts of the application
 module.exports = Review;
--- models/cartModel.js
+++ models/cartModel.js
@@ -1,31 +1,36 @@
 const mongoose = require("mongoose");
 
+// Define the cart schema using Mongoose
 const cartSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+    ref: "user", // Reference to the "user" model
     required: true,
   },
+  // Array of items in the cart, each item includes productId and quantity
   items: [
     {
       productId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
+        ref: "Product", // Reference to the "Product" model
         required: true,
       },
       quantity: {
         type: Number,
         required: true,
-        min: 1,
+        min: 1, // Ensure quantity is at least 1
       },
     },
   ],
+  // Creation date of the cart, defaults to the current date and time
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
+// Create a Mongoose model named "Cart" based on the cartSchema
 const cartModel = mongoose.model("Cart", cartSchema);
 
+// Export the Cart model to be used in other parts of the application
 module.exports = cartModel;
